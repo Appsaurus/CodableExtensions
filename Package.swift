f35b8423e0ad9950fcc3aa8a8a6552089304d1f5
Brian Strobach <brian@appsaurus.io>
2019-06-06 08:59:27 -0500

    Setting platform deployment targets.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
 	name: "CodableExtensions",
+    platforms: [
+        .macOS(.v10_12), .iOS(.v10),
+    ],
 	products: [
 		.library(name: "CodableExtensions", targets: ["CodableExtensions"])
 	],
